aa5f4a9800a56e709d818abbb4bb8516932e4954
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-01-31 14:29:48 +0900

    chore: react router dom 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 
-import App from './App.tsx';
-
 import { theme } from './styles/theme.ts';
 import { global } from './styles/global.ts';
+
+import Router from './router/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Global styles={global} />
-      <App />
+      <Router />
     </ThemeProvider>
   </React.StrictMode>,
 );
