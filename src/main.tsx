4c60a01a239a31b2958c09fbf4deddfd3bddd9a9
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-01 14:59:15 +0900

    chore: rtk, react-redux 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { Global, ThemeProvider } from '@emotion/react';
 
 import { theme } from './styles/theme.ts';
 import { global } from './styles/global.ts';
 
 import Router from './router/index.tsx';
+import { store } from './store/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Global styles={global} />
-      <Router />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Global styles={global} />
+        <Router />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
