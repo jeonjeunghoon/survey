cc073eb96d5cbc1f67d68c5af27fba0e2d5e0f48
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-03 23:26:50 +0900

    fix: 기타 추가 에러 해결

--- src/constants/option.ts
+++ src/constants/option.ts
@@ -5,4 +5,4 @@ export const isRenderAnswer = (type: QuestionType) =>
   type === QUESTION_TYPE.단답형 || type === QUESTION_TYPE.장문형;
 
 export const isRenderOtherOptionButton = (type: QuestionType, hasOtherOption: boolean) =>
-  (!hasOtherOption && type === QUESTION_TYPE.객관식질문) || type === QUESTION_TYPE.체크박스;
+  !hasOtherOption && (type === QUESTION_TYPE.객관식질문 || type === QUESTION_TYPE.체크박스);
